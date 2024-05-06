f14154d9c76ddace5d000293f586b1cfc4a0fa53
Nebrok <nebrok.red@outlook.com>
2024-05-06 17:12:06 +0200

    Adds optimised bubble sort and draws both sorts to the screen. Fixed bug where data variable within class references wrong "data".

--- Sort_Algos.js
+++ Sort_Algos.js
@@ -1,13 +1,13 @@
 class BubbleSort {
-    constructor(centre, width, height, data) {
+    constructor(centre, width, height, list_data) {
         this._centre = centre
         this._width = width
         this._half_width = width/2
         this._height = height
         this._half_height = height/2
 
-        this._input_data = data
-        this._data = data
+        this._input_data = list_data
+        this._data = list_data
 
         this._bar_width = width / data.length
 
@@ -23,6 +23,7 @@ class BubbleSort {
         noStroke()
         let left_side_x = this._centre.x - this._half_width;
         let bottom_y = this._centre.y + this._half_height;
+
         for(let i = 0; i < this._data.length; i++) {
             let bar_height = map(this._data[i], 0, 1, 0, this._height)
             if(this._current_index == i) {
@@ -37,10 +38,10 @@ class BubbleSort {
         if (!this._is_sorted) {
             this._iteration_count++;
 
-            if (data[this._current_index-1] > data[this._current_index]) {
-                let current = data[this._current_index];
-                data[this._current_index] = data[this._current_index-1];
-                data[this._current_index-1] = current;
+            if (this._data[this._current_index-1] > this._data[this._current_index]) {
+                let current = this._data[this._current_index];
+                this._data[this._current_index] = this._data[this._current_index-1];
+                this._data[this._current_index-1] = current;
 
                 this._swap_count++;
                 this._swapped = true;
@@ -52,7 +53,7 @@ class BubbleSort {
                 //Check if the list is sorted
                 if (this._swapped == false) {
                     this._is_sorted = true
-                    console.log("Sorted")
+                    console.log("BubbleSort complete")
                     console.log("Number of iterations: " + this._iteration_count)
                     console.log("Number of swaps: " + this._swap_count)
                 }
@@ -68,10 +69,10 @@ class BubbleSort {
             swapped = false
             for(let i = 1; i < this._data.length; i++) {
                 this._iteration_count++;
-                if (data[i-1] > data[i]) {
-                    let current = data[i];
-                    data[i] = data[i-1];
-                    data[i-1] = current;
+                if (this._data[i-1] > this._data[i]) {
+                    let current = this._data[i];
+                    this._data[i] = this._data[i-1];
+                    this._data[i-1] = current;
                     swapped = true
 
                     this._swap_count++;
@@ -83,7 +84,7 @@ class BubbleSort {
         let end_time = millis()
         let total_time_secs = (end_time - start_time) / 1000
 
-        console.log("Sorted in " + total_time_secs + " seconds")
+        console.log("BubbleSort complete in " + total_time_secs + " seconds")
         console.log("Number of iterations: " + this._iteration_count)
         console.log("Number of swaps: " + this._swap_count)
         
@@ -93,5 +94,71 @@ class BubbleSort {
         return this._is_sorted;
     }
 
+}
+
+class OptimisedBubbleSort extends BubbleSort {
+    constructor(centre, width, height, data) {
+        super(centre, width, height, data);
+
+        this._number_items = this._data.length
+    }
+
+    step() {
+        if (!this._is_sorted) {
+            this._iteration_count++;
+
+            if (this._data[this._current_index-1] > this._data[this._current_index]) {
+                let current = this._data[this._current_index];
+                this._data[this._current_index] = this._data[this._current_index-1];
+                this._data[this._current_index-1] = current;
+
+                this._swap_count++;
+                this._swapped = true;
+            }
+
+            this._current_index++;
+            if (this._current_index == this._number_items) {
+                this._current_index = 1;
+                this._number_items--;
+                //Check if the list is sorted
+                if (this._swapped == false) {
+                    this._is_sorted = true
+                    console.log("OptimisedBubbleSort Complete")
+                    console.log("Number of iterations: " + this._iteration_count)
+                    console.log("Number of swaps: " + this._swap_count)
+                }
+                this._swapped = false;
+            }
+        };
+    }
+
+    sort() {
+        let list_length = this._data.length
+        let start_time = millis()
+        let swapped = false;
+        do {
+            swapped = false
+            for(let i = 1; i < list_length; i++) {
+                this._iteration_count++;
+                if (this._data[i-1] > this._data[i]) {
+                    let current = this._data[i];
+                    this._data[i] = this._data[i-1];
+                    this._data[i-1] = current;
+                    swapped = true
+
+                    this._swap_count++;
+                }
+            }
+            list_length--;
+        } while (swapped);
+        this._is_sorted = true;
+
+        let end_time = millis()
+        let total_time_secs = (end_time - start_time) / 1000
+
+        console.log("OptimisedBubbleSort complete in " + total_time_secs + " seconds")
+        console.log("Number of iterations: " + this._iteration_count)
+        console.log("Number of swaps: " + this._swap_count)
+    }
 }
 
--- sketch.js
+++ sketch.js
@@ -1,21 +1,27 @@
 
-let size = [900, 600];
+let size = [900, 800];
 
 let centre;
 let sortAlgo;
 
 let data = [];
+let data2 = [];
 
 function setup() {
   createCanvas(size[0], size [1]);
   background(75)
 
-  for(let i = 0; i < 10000; i++) {
-    data.push(random());
+  for(let i = 0; i < 100; i++) {
+    data_item = random()
+    data.push(data_item);
+    data2.push(data_item);
   }
 
-  centre = createVector(size[0]/2, size[1]/2);
-  sortAlgo = new BubbleSort(centre, size[0] - 100, size[1] - 100, data);
+  topCentre = createVector(size[0]/2, size[1]/4);
+  bottomCentre = createVector(size[0]/2, size[1]/4 + size[1]/2)
+
+  optimisedBubble = new OptimisedBubbleSort(bottomCentre, size[0] - 100, (400 - 100), data);
+  bubble = new BubbleSort(topCentre, size[0] - 100, size[1]/2 - 100, data2)
   frameRate(60)
 
 }
@@ -23,13 +29,17 @@ function setup() {
 function draw() {
   background(75);
   
-  //for (let count = 0; count < 1000; count++) {
-  //  sortAlgo.step()
-  //}
-  sortAlgo.sort();
-  if (sortAlgo.is_sorted() == true) {
+  for (let count = 0; count < 10; count++) {
+    bubble.step();
+    optimisedBubble.step();
+  }
+  //bubble.sort();
+  //optimisedBubble.sort();
+  
+  if (bubble.is_sorted() == true) {
     noLoop()
   }
-  sortAlgo.draw();
+  bubble.draw();
+  optimisedBubble.draw();
   
 }
