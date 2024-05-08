6006db9744130f88dcf1d1465ca37367084c0cc0
Nebrok <nebrok.red@outlook.com>
2024-05-08 22:27:56 +0200

    Added Merge sort. No check to see if merge sort finished yet

--- Sort_Algos.js
+++ Sort_Algos.js
@@ -162,3 +162,228 @@ class OptimisedBubbleSort extends BubbleSort {
     }
 }
 
+class TopDownMergeSort {
+    constructor(centre, width, height, list_data) {
+        this._centre = centre
+        this._width = width
+        this._half_width = width/2
+        this._height = height
+        this._half_height = height/2
+
+        this._bar_width = width / data.length
+
+        this._input_data = list_data
+        this._data = list_data
+
+        this._aux_list = [list_data.slice(0)]
+        this._current_index = 1
+        this._split_status = "Incomplete"
+        this._iteration_count = 0
+        this._beginning_of_merge = 0
+
+        this._word_index = 0
+        this._working_list = []
+        this._consolidated_on_last = true
+
+        this._is_sorted = false
+
+        this._number_swaps = 0
+
+
+    }
+
+    step() {
+
+        if (this._split_status == "Incomplete") {
+            if (this._iteration_count != 0) {this._aux_list = this._data.slice(0)}
+
+            this._aux_list = this.split_lists(this._aux_list)
+            if (this._aux_list.length == this._input_data.length) {
+                this._split_status = "Complete"
+                this._beginning_of_merge = this._iteration_count + 1
+            }
+            this._data = this._aux_list.slice(0)
+            this._aux_list = []
+        }
+        else if (this._split_status == "Complete") {
+            if (this._iteration_count == this._beginning_of_merge) {
+                this._aux_list = this._data.slice(this._word_index)
+            } else if (!this._consolidated_on_last){
+                this._aux_list = this._data.slice(this._word_index + 1)
+            } else {
+                this._aux_list = this._data.slice(this._word_index)
+            }
+
+            //console.log("Aux list length " + this._aux_list.length)
+
+            if (this._aux_list.length > 1) {
+                let left_side = this._aux_list[0]
+                let right_side = this._aux_list[1]
+                
+                if (left_side[0] <= right_side[0]) {
+                    this._working_list.push(left_side[0])
+                    left_side = left_side.slice(1)
+                    this._number_swaps++;
+                } else {
+                    this._working_list.push(right_side[0])
+                    right_side = right_side.slice(1)
+                    this._number_swaps++;
+                }
+
+                if (left_side.length != 0 && right_side.length != 0) {
+                    this._aux_list[0] = left_side
+                    this._aux_list[1] = right_side
+
+                    let sorted_from_data = this._data.slice(0,this._word_index)
+                    //console.log(sorted_from_data)
+                    //console.log(this._working_list)
+                    let remaining_aux = this._aux_list.slice(0)
+                    //console.log(remaining_aux)
+                    this._data = sorted_from_data.concat([this._working_list]).concat(remaining_aux)
+                    //console.log(this._data)
+
+                    this._consolidated_on_last = false
+
+                } else {
+                    while (left_side.length != 0) {
+                        this._working_list.push(left_side[0])
+                        left_side = left_side.slice(1)
+                    }
+                    while (right_side.length != 0) {
+                        this._working_list.push(right_side[0])
+                        right_side = right_side.slice(1)
+                    }
+                    
+                    let sorted_from_data = this._data.slice(0,this._word_index)
+                    //console.log(sorted_from_data)
+                    //console.log(this._working_list)
+                    let offset = 0
+                    if (!this._consolidated_on_last) {
+                        offset = 1
+                    }
+                    let untouch_from_aux = this._data.slice(this._word_index + 2 + offset)
+                    //console.log(untouch_from_aux)
+                    this._data = sorted_from_data.concat([this._working_list]).concat(untouch_from_aux)
+
+                    this._consolidated_on_last = true
+
+                    this._word_index += 1;
+                    this._working_list = []
+                    //console.log(this._data)
+                    //console.log("New runthrough")
+                }   
+            } else {
+                this._word_index = 0
+                //console.log("New runthrough")
+            }
+        }
+        this._iteration_count++;
+
+        /*///// HERHEHE
+        Think it works, need to figure out how to check if sorted, also need to draw it
+        therefore flatten whatever is in the data property then draw.
+        */
+    }
+
+    split_lists(lists_to_split) {
+        let result = []
+        for (let i = 0; i < lists_to_split.length; i++) {
+
+            let left = []
+            let right = []
+            let middle = lists_to_split[i].length / 2
+            for (let j = 0; j < lists_to_split[i].length; j++) {
+                if (j < middle) {
+                    left.push(lists_to_split[i][j])
+                } else {
+                    right.push(lists_to_split[i][j])
+                }
+            }
+            if (left.length != 0) {
+                result.push(left)
+            }
+            if (right.length != 0) {
+                result.push(right)
+            }
+        }
+        return result
+    }
+
+    draw() {
+        let flattened_data = this._data.flat()
+        noStroke()
+        let left_side_x = this._centre.x - this._half_width;
+        let bottom_y = this._centre.y + this._half_height;
+
+        for(let i = 0; i < flattened_data.length; i++) {
+            let bar_height = map(flattened_data[i], 0, 1, 0, this._height)
+            if(this._current_index == i) {
+                fill(0, 255, 0);
+            }
+            rect(left_side_x + this._bar_width*i, bottom_y - bar_height, this._bar_width, bar_height);
+            fill(255,255,255);
+        }
+    }
+
+    sort() {
+        this._number_swaps = 0
+        let start_time = millis()
+        this._data = this.merge_sort(this._data)
+
+        let end_time = millis()
+        let total_time_secs = (end_time - start_time) / 1000
+
+        console.log("MergeSort complete in " + total_time_secs + " seconds")
+        console.log("Total Number of swaps: " + this._number_swaps)
+    }
+
+    merge_sort(data) {
+        if (data.length <= 1) {
+            return data
+        }
+
+        let left = []
+        let right = []
+        let middle = data.length / 2
+        for (let i = 0; i < data.length; i++) {
+            if (i < middle) {
+                left.push(data[i])
+            } else {
+                right.push(data[i])
+            }
+        }
+
+        left = this.merge_sort(left)
+        right = this.merge_sort(right)
+
+        return this.merge(left, right)
+
+    }
+
+    merge(left, right) {
+        var result = []
+
+        while (left.length != 0 && right.length != 0) {
+            if (left[0] <= right[0]) {
+                result.push(left[0])
+                left = left.slice(1)
+                this._number_swaps++;
+            } else {
+                result.push(right[0])
+                right = right.slice(1)
+                this._number_swaps++;
+            }
+        }
+
+        while (left.length != 0) {
+            result.push(left[0])
+            left = left.slice(1)
+        }
+        while (right.length != 0) {
+            result.push(right[0])
+            right = right.slice(1)
+        }
+
+        return result
+    }
+}
--- sketch.js
+++ sketch.js
@@ -6,40 +6,56 @@ let sortAlgo;
 
 let data = [];
 let data2 = [];
+let data3 = []
 
 function setup() {
   createCanvas(size[0], size [1]);
   background(75)
+  randomSeed()
 
-  for(let i = 0; i < 100; i++) {
+  for(let i = 0; i < 128; i++) {
     data_item = random()
-    data.push(data_item);
-    data2.push(data_item);
+    data.push(random());
+    data2.push(random());
+    data3.push(data_item);
+
   }
+
+  let reversed_ordered_data = data.sort().reverse()
 
   topCentre = createVector(size[0]/2, size[1]/4);
   bottomCentre = createVector(size[0]/2, size[1]/4 + size[1]/2)
 
   optimisedBubble = new OptimisedBubbleSort(bottomCentre, size[0] - 100, (400 - 100), data);
   bubble = new BubbleSort(topCentre, size[0] - 100, size[1]/2 - 100, data2)
-  frameRate(60)
 
+  mergeSort2 = new TopDownMergeSort(topCentre, size[0] - 100, size[1]/2 - 100, reversed_ordered_data)
+  mergeSort = new TopDownMergeSort(bottomCentre, size[0] - 100, (400 - 100), data3)
+  frameRate(60)
 }
 
 function draw() {
   background(75);
   
+  /*
   for (let count = 0; count < 10; count++) {
     bubble.step();
     optimisedBubble.step();
   }
+  */
   //bubble.sort();
   //optimisedBubble.sort();
-  
-  if (bubble.is_sorted() == true) {
-    noLoop()
-  }
-  bubble.draw();
-  optimisedBubble.draw();
-  
+
+  //bubble.draw();
+  //optimisedBubble.draw();
+
+  //mergeSort.sort()
+  //mergeSort.draw()
+  //bubble.step();
+  //bubble.draw();
+
+  mergeSort.step()
+  mergeSort2.step()
+  mergeSort.draw()
+  mergeSort2.draw()
 }
